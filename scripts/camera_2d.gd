4d227685ac534df1d2399df5b8cb23b66954f3ff
Pkb-Codes <krishnabhargavpuli@gmail.com>
2026-04-23 18:06:26 +0530

    dynamic camera added
    added a dynamic camera that moves and adjusts zoom based in the players position

---
+++ scripts/camera_2d.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+var player1
+var player2
+
+func _ready():
+	player1 = get_parent().get_node("player1")
+	player2 = get_parent().get_node("player2")
+
+func _process(delta: float) -> void:
+	global_position = (player1.global_position + player2.global_position)/2
+	var distance = player1.global_position.distance_to(player2.global_position)
+	
+	if distance > 400:
+		var zoom_factor = 1.2 - clamp(distance / 2000, 0, 0.8)
+		zoom = Vector2(zoom_factor, zoom_factor)
